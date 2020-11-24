4cc858600bec0707b1d068089b86db8cbd187498
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2020-11-24 09:12:33 +0100

    Summary

--- woocommerce/single-product-parar-de-pensar.php
+++ woocommerce/single-product-parar-de-pensar.php
@@ -86,42 +86,246 @@
       </div>
     </section>
     <section class="showcase2">
-        <div class="gali">
-            <img class="gali_img" src="https://picsum.photos/300" alt="">
-            <img class="gali_img" src="https://picsum.photos/301" alt="">
-            <img class="gali_img" src="https://picsum.photos/302" alt="">
-            <img class="gali_img" src="https://picsum.photos/303" alt="">
-        </div>
-        <div class="pista">
-            <h5 class="pista_title">Programa Orientativo</h5>
+      <div class="gali">
+        <img class="gali_img" src="https://picsum.photos/300" alt="">
+        <img class="gali_img" src="https://picsum.photos/301" alt="">
+        <img class="gali_img" src="https://picsum.photos/302" alt="">
+        <img class="gali_img" src="https://picsum.photos/303" alt="">
+      </div>
+      <div class="pista">
+        <h5 class="pista_title">Programa Orientativo</h5>
 
-            <?php $i = 1;
-            while(true){
-                if(get_post_meta(get_the_ID(), 'G_programa_'.$i.'_text', true)){ ?>
-                    <div class="pista_group">
-                        <p class="pista_txt pista_time"><?php echo get_post_meta(get_the_ID(), 'G_programa_'.$i.'_time', true); ?></p>
-                        <p class="pista_txt pista_text"><?php echo get_post_meta(get_the_ID(), 'G_programa_'.$i.'_text', true); ?></p>
-                    </div>
-                    <?php
-                    $i=$i+1;
-                } else { break; }
-            } ?>
-            <div class="pista_deco"></div>
-            <p class="pista_caption">El retiro consta de unas 20 horas formativas, dejando el sábado por la tarde libre. (No se sirve cena la noche del sábado).
-            </p>
-        </div>
+        <?php $i = 1;
+        while(true){
+          if(get_post_meta(get_the_ID(), 'G_programa_'.$i.'_text', true)){ ?>
+            <div class="pista_group">
+              <p class="pista_txt pista_time font_size_6"><?php echo get_post_meta(get_the_ID(), 'G_programa_'.$i.'_time', true); ?></p>
+              <p class="pista_txt pista_text font_size_6"><?php echo get_post_meta(get_the_ID(), 'G_programa_'.$i.'_text', true); ?></p>
+            </div>
+            <?php
+            $i=$i+1;
+          } else { break; }
+        } ?>
+        <div class="pista_deco"></div>
+        <p class="pista_caption">El retiro consta de unas 20 horas formativas, dejando el sábado por la tarde libre. (No se sirve cena la noche del sábado).
+        </p>
+      </div>
     </section>
     <section class="col_2_block">
       <h3 class="col_2_block_title font_size_3"><?php echo get_post_meta(get_the_ID(), 'H_segunda_descripcion_titulo', true); ?></h3>
       <div class="col_2_block_col">
         <p class="col_2_bloct_txt font_size_5"><?php echo get_post_meta(get_the_ID(), 'I_segunda_descripcion_texto_1', true); ?></p>
-        <p class="col_2_bloct_txt font_size_5"></p>
+        <p class="col_2_bloct_txt font_size_5"><?php echo get_post_meta(get_the_ID(), 'I_segunda_descripcion_texto_2', true); ?></p>
       </div>
       <div class="col_2_block_col">
-        <p class="col_2_bloct_txt font_size_5"><strong>¿Qué incluye?</strong> <br> Retiro, 3 noches de alojamiento, comidas y cenas durante la estancia excepto la cena después de la tarde libre, formación a distancia de 8 semanas, servicio de búsqueda al aeropuerto de Santander o estación de autobuses/ tren en Cantabria. No incluye transporte a Cantabria. </p>
-        <p class="col_2_bloct_txt font_size_5">Creado e impartido por <a class="content_link" href="#">Geoffrey molloy</a> & <a class="content_link" href="#">Rhea Sivi</a> (clica en los nombres para saber más)</p>
+        <p class="col_2_bloct_txt font_size_5"><?php echo get_post_meta(get_the_ID(), 'I_segunda_descripcion_texto_3', true); ?></p>
+        <p class="col_2_bloct_txt font_size_5"><?php echo get_post_meta(get_the_ID(), 'I_segunda_descripcion_texto_4', true); ?></p>
       </div>
     </section>
+
+
+
+    <section class="showcase3">
+      <h3 class="showcase_title font_size_3 simple_title hosi_color_85AFCA">Herramientas que te servirán para...</h3>
+
+
+      <div class="hosi hosi_color_85AFCA">
+        <?php include get_template_directory().'/assets/shield.svg'; ?>
+        <p class="hosi_txt font_size_5">Reducir estrés y ansiedad.</p>
+      </div>
+
+      <div class="hosi hosi_color_85AFCA">
+        <?php include get_template_directory().'/assets/emotional_intelligence.svg'; ?>
+        <p class="hosi_txt font_size_5">
+          Aumentar la inteligencia emocional.
+        </p>
+      </div>
+
+      <div class="hosi hosi_color_85AFCA">
+        <?php include get_template_directory().'/assets/sleepy_moon.svg'; ?>
+        <p class="hosi_txt font_size_5">
+          Crear un sueño más profundo y reparador.
+        </p>
+      </div>
+
+      <div class="hosi hosi_color_85AFCA">
+        <?php include get_template_directory().'/assets/map_compose.svg'; ?>
+        <p class="hosi_txt font_size_5">
+          Ver “the big picture” para poder apreciar mejor los detalles.
+        </p>
+      </div>
+
+      <div class="hosi hosi_color_85AFCA">
+        <?php include get_template_directory().'/assets/listening_ear.svg'; ?>
+        <p class="hosi_txt font_size_5">
+          Mejorar la escucha activa, la concentración y creatividad.
+        </p>
+      </div>
+
+      <div class="hosi hosi_color_85AFCA">
+        <?php include get_template_directory().'/assets/perceptive_brain.svg'; ?>
+        <p class="hosi_txt font_size_5">
+          Crear una percepción flexible para mejorar la respuesta ante diferentes situaciones.
+        </p>
+      </div>
+
+      <div class="hosi hosi_color_85AFCA">
+        <?php include get_template_directory().'/assets/opportunity.svg'; ?>
+        <p class="hosi_txt font_size_5">
+          Afrontar el cambio y aprovechar las oportunidades que nos brinda.
+        </p>
+      </div>
+
+      <div class="hosi hosi_color_85AFCA">
+        <?php include get_template_directory().'/assets/relationship.svg'; ?>
+        <p class="hosi_txt font_size_5">
+          Mejorar las relaciones interpersonales.
+        </p>
+      </div>
+
+      <div class="hosi hosi_color_85AFCA">
+        <?php include get_template_directory().'/assets/happy_brain.svg'; ?>
+        <p class="hosi_txt font_size_5">
+          Hacernos más resistentes a la depresión.
+        </p>
+      </div>
+    </section>
+
+
+
+    <section class="copa">
+      <img class="copa_img" src="https://picsum.photos/400" alt="">
+      <div class="copa_interaction_container">
+        <h5 class="copa_title">Retiro en Cantabria 3 días<br><?php the_title(); ?></h5>
+        <p class="copa_label">FECHAS</p>
+
+        <div class="copa_select_container">
+          <select class="copa_select" name="" id="">
+            <option value="">Elige una opcion</option>
+          </select>
+          <svg class="copa_select_arrow" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+            <g id="arrow_select">
+              <path d="M0 0h24v24H0V0z" fill="white"/>
+              <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" fill="currentColor"/>
+            </g>
+          </svg>
+        </div>
+
+        <?php
+        // $product = wc_get_product();
+        if ( $product->is_type( 'variable' ) AND !$is_out_of_stock ) {
+          $variations = $product->get_available_variations();
+
+          // THIS BLOCK DEFINES THE ARRAY: "$myAttributes"
+          // WE WILL USE THIS ARRAY LATER
+          $myAttributes = array();
+          foreach ($variations as $key => $value) {
+            foreach ($value['attributes'] as $j => $var) {
+              if ( ! array_key_exists ( $j, $myAttributes ) ) {
+                $myAttributes[$j] = array($var => array($value['variation_id']));
+              } else {
+                $myAttributes[$j][$var][] = $value['variation_id'];
+              }
+            }
+          }
+          // var_dump($myAttributes);
+
+          $first = true;
+          foreach ($myAttributes as $key => $value) {
+            $slug = preg_replace("/attribute_/i", "", $key);
+            $name = ucfirst($slug);
+            ?>
+
+            <div class="SelectBox copa_select" tabindex="1" id="selectBox<?php echo $name; ?>">
+              <div class="selectBoxButton" onclick="altClassFromSelector('focus', '#selectBox<?php echo $slug; ?>')">
+                <!-- <p class="selectBoxPlaceholder"><?php // _e('elige una opcion', 'lt_domain'); ?></p> -->
+                <p class="selectBoxPlaceholder"><?php echo $name; ?></p>
+                <p class="selectBoxCurrent" id="selectBoxCurrent<?php echo $name; ?>"></p>
+              </div>
+              <div class="selectBoxList">
+                <label for="nul<?php echo $name; ?>" class="selectBoxOption">
+                  <input
+                  class="selectBoxInput"
+                  id="nul<?php echo $name; ?>"
+                  type="radio"
+                  data-ids=""
+                  name="filter_<?php echo $slug; ?>"
+                  onclick="selectBoxControler('','#selectBox<?php echo $name; ?>','#selectBoxCurrent<?php echo $name; ?>')"
+                  value="0"
+                  checked
+                  >
+                  <!-- <span class="checkmark"></span> -->
+                  <p class="colrOptP"></p>
+                </label>
+                <?php foreach ($value as $i => $var) { ?>
+                  <!-- <p class="colrOptP"><?php // var_dump($var['attributes']); ?></p> -->
+                  <?php // foreach ($value['options'] as $key => $var) { ?>
+                    <label for="<?php echo $i; ?>" class="selectBoxOption<?php if(!$first){echo ' hidden';} ?>">
+                      <input
+                      class="selectBoxInput"
+                      id="<?php echo $i; ?>"
+                      data-ids="<?php
+                      foreach ($var as $j => $x) {
+                        echo $x;
+                        if($j<count($var)-1){ echo ', '; }
+                      }
+                      ?>"
+                      type="radio"
+                      name="filter_<?php echo $slug; ?>"
+                      onclick="selectBoxControler('<?php echo $i; ?>', '#selectBox<?php echo $name; ?>', '#selectBoxCurrent<?php echo $name; ?>')"
+                      value="<?php echo $i; ?>"
+                      <?php // if($_GET['filter_'.$term->slug]==$var->slug){echo "selected";} ?>
+                      >
+                      <!-- <span class="checkmark"></span> -->
+                      <!-- <p class="colrOptP"><?php // var_dump($var[0]); ?></p> -->
+                      <p class="colrOptP"><?php echo $i; ?></p>
+                    </label>
+                  <?php } ?>
+                </div>
+              </div>
+              <?php $first = false; ?>
+            <?php } ?>
+          <?php } ?>
+
+
+
+          <!-- <p class="copa_price">475,00€</p> -->
+          <p class="copa_price" id="singleSidePrice"><?php echo $product->get_price_html(); ?></p>
+
+          <div class="cuantos Cuantos">
+            <button class="cuantosBtn cuantosMins">-</button>
+            <input class="cuantosQnt cuantosQantity" type="text" value="1" min="1">
+            <button class="cuantosBtn cuantosPlus">+</button>
+          </div>
+
+          <button
+          class="btn"
+          id="myAddToCart"
+          data-product-id="<?php echo get_the_id(); ?>"
+          data-product-type="<?php echo $product->get_type(); ?>"
+          data-quantity="1"
+          data-variation-description=""
+          data-buy="later"
+          <?php if($product->is_on_backorder()){ ?>
+            data-preorder="true"
+          <?php } else { ?>
+            data-preorder="false"
+          <?php } ?>
+          <?php if( $is_out_of_stock ){ echo 'disabled'; } ?>
+          >
+          <?php
+          if( $is_out_of_stock ){
+            echo 'Sin cupo';
+          } else {
+            echo 'Añadir a la cesta';
+          }
+          ?>
+        </button>
+
+        <p class="copa_mas">Más información <a class="copa_mas_link" href="#">CLICA AQUÍ</a></p>
+      </div>
+    </section>
+
 
 
   <?php } ?>
