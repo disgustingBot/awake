55d4a9c6259ca5fe8ee41f7423675e6de0b40ac5
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2021-02-12 20:01:06 +0100

    Update

--- page-contacto.php
+++ page-contacto.php
@@ -12,6 +12,35 @@
       <input class="main_form_input font_size_8" type="text" placeholder="Apellidos">
       <input class="main_form_input font_size_8" type="email" placeholder="Email*">
       <input class="main_form_input font_size_8" type="tel" placeholder="Telf">
+      <select class="main_form_input font_size_8" name="" id="">
+
+        <?php
+        $args = array(
+          'post_type' => 'product',
+          'posts_per_page' => -1,
+          'tax_query' => array(
+            array(
+              'taxonomy' => 'product_cat',
+              'field' => 'slug',
+              'terms'=> array('programas', 'empresas'),
+            )
+          ),
+        );
+         $programas = new WP_Query($args);
+
+         if ($programas -> have_posts()) {
+           while ($programas -> have_posts()) {$programas -> the_post();
+           ?>
+             <option value=""><?php the_title() ?></option>
+           <?php }  wp_reset_query();
+         } ?>
+
+
+
+
+
+
+      </select>
       <textarea class="main_form_input font_size_8 main_form_textarea" name="" id="" placeholder="Escríbenos*"></textarea>
       <input class="main_form_btn btn" type="submit" value="Enviar">
     </form>
