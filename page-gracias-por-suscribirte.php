d8971fab67ad7504e8d37b2bd9b8711165875ef9
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2021-10-14 13:02:07 +0200

    Summary

---
+++ page-gracias-por-suscribirte.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div class="thanks_body">
+  <div class="thanks_container">
+    <p class="showcase_title"><strong>Suscripción realizada con éxito</strong></p>
+    <p class="showcase_title">¡Gracias por suscribirte a nuestro Newsletter!</p>
+    <p class="showcase_title">Visita nuestro blog si quieres disfrutar de todo nuestro contenido.</p>
+    <a class="font_size_8 btn" href="https://esfacilsisabescomo.com/blog">Ir al Blog</a>
+  </div>
+</div>
+<?php get_footer(); ?>
